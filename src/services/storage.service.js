1ffbd7706a7a71b4dca1c92e981b2de72fc1acaa
yadav767 <yyadavaayush767@gmail.com>
2026-02-26 02:51:16 +0530

    Move imagekit in function

--- src/services/storage.service.js
+++ src/services/storage.service.js
@@ -1,11 +1,13 @@
 const ImageKit = require('@imagekit/nodejs');
 
 
-const imagekit = new ImageKit({
-    privateKey: process.env.IMAGEKIT_API_KEY,
-});
+
 
 async function uploadFile(file, fileName) {
+
+    const imagekit = new ImageKit({
+        privateKey: process.env.IMAGEKIT_API_KEY,
+    });
     const result = await imagekit.files.upload({
         file: file.toString("base64"),
         fileName: fileName
