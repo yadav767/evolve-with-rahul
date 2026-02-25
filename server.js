b5a942394aafde2a0235b974e492efb8d3f43b03
yadav767 <yyadavaayush767@gmail.com>
2026-02-26 02:35:12 +0530

    Add NODE_ENV

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ connectDB()
 
 
 const PORT=process.env.PORT || 3000
-app.listen(PORT,()=>{
-    console.log("server is running at the port 3000");
-})
+if (process.env.NODE_ENV !== "production") {
+    app.listen(PORT, () => {
+        console.log("server is running at the port 3000")
+    })
+}
