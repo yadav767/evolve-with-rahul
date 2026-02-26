fbf4a1dcdc41f539a083909d26756202b19653f8
yadav767 <yyadavaayush767@gmail.com>
2026-02-27 01:17:03 +0530

    kuch to kiya hai

--- src/services/mail.service.js
+++ src/services/mail.service.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async (to, subject, text, html) => {
     try {
         const info = await resend.emails.send({
-            from: `Evolve With Rahul <${process.env.USER_GMAIL}>`, 
+            from: 'Evolve With Rahul <onboarding@resend.dev>',
             to,
             subject,
             text,
