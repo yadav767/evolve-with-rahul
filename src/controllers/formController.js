5a1f4aec12be8b347c03bb5822b94115462930cf
yadav767 <yyadavaayush767@gmail.com>
2026-02-27 02:29:05 +0530

    galti ho gayi

--- src/controllers/formController.js
+++ src/controllers/formController.js
@@ -10,7 +10,6 @@ async function submitFormController(req, res) {
 
         
         await sendEmailToInstructor(newUser)
-        // await sendEmailToUser(newUser)
 
         res.status(200).json({
             message: "Mail send successfully !",
